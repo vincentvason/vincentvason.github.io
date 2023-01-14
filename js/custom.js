715ad4a724c51911b965317fb6df2f96134de9b9
Vason Maitree <vason.mi3@gmail.com>
2023-01-14 19:55:48 +0000

    Update Game Portfolio 2023

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+var myNav = $("#nav");
+$(".panel.top").css("background-color", "black");
+
+// $(window).on('scroll', function() {
+//   "use strict";
+// //   if ($(window).scrollTop() >= 280) {
+//     $(".panel.top").css("background-color", "black");
+// //   } else {
+//     // $(".panel.top").css("background-color", "transperent");
+// //   }
+// });
